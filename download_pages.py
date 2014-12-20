18a169e04f51e6d323d04f158ca59df3ae6c4db5
Matthew Turner <maturner01@gmail.com>
2014-12-19 23:12:43 -0500

    download_pages downloading pages; also has a dictionary maker: stripped contract paragraphs keyed by defense agency, e.g. army, navy, etc

--- download_pages.py
+++ download_pages.py
@@ -6,28 +6,63 @@
 #
 # contracts start at number 391 (October 07, 1994)
 #
-from __future__ import unicode_literals
+import os
+
 from urllib2 import urlopen
+from bs4 import BeautifulSoup
+from collections import defaultdict
+from json import dumps
 
-url_root = 'http://www.defense.gov/contracts/contract.aspx?contractid='
+def download_announcements(first_id=1842, last_id=5442):
+    """
+    Download contract announcements for a range of contract IDs from
+    http://www.defense.gov/Contracts
+    """
+    url_root = 'http://www.defense.gov/contracts/contract.aspx?contractid='
 
-# as of Dec 19 (TODO: Generalize to look up: go to www.defense.gov/contracts)
-current_id = 5442
-# TODO: accept this as a command line parameter
-days_back = 3650  # (ten years)
-oldest_id = current_id - days_back
+    save_dir = "data/html/"
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
 
-ids = [str(valid_num) for valid_num in range(5442, 1842)]
+    # download newest first because that's handier
+    ids = [str(valid_num) for valid_num in range(last_id, first_id, -1)]
 
-# for id_ in ids[-5:]:
-for id_ in ids:
+    # for id_ in ids[-5:]:
+    for id_ in ids:
 
-    url = url_root + id_
-    page = urlopen(url)
-    html = page.read()
+        url = url_root + id_
+        page = urlopen(url)
+        html = page.read()
 
-    save_file = "data/raw_html" + id_ + ".html"
-    with open(save_file, 'w') as f:
-        f.write(html)
+        save_file = save_dir + id_ + ".html"
+        with open(save_file, 'w') as f:
+            f.write(html)
 
-    print("Downloaded ", url)
+        print "Downloaded ", url
+
+
+def make_agency_dict(html_file, save_file=None):
+    """
+    Parse the defense.gov contract html
+    """
+    soup = BeautifulSoup(open(html_file, 'r'))
+
+    # the first paragraph says "CONTRACTS"
+    pars = soup.find_all("p")[1:]
+
+    contract_dict = defaultdict(list)
+    for p in pars:
+        # contracting branches are styled (with centering)
+        if 'style' in p.attrs.keys():
+            branch = p.get_text().strip().lower()
+            contract_dict[branch] = []
+        else:
+            text = p.get_text().strip()
+            if text:
+                contract_dict[branch].append(text)
+
+    if save_file:
+        with open(save_file, 'w+') as f:
+            f.write(dumps(contract_dict))
+
+    return contract_dict
