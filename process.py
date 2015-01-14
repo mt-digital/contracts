df98d3296f9b5595bb41c63e713b90a5f361c14d
Matthew Turner <maturner01@gmail.com>
2015-01-13 22:03:53 -0700

    added count_by_activity to process.py

--- process.py
+++ process.py
@@ -1,3 +1,13 @@
+"""
+Module: process.py
+
+For various processing tasks of contract data.
+
+Definitions:
+    Activity- A DoD technical term that I'm currently (mis)using to refer to
+              subgroups within the DoD, e.g. Army, Navy, Defense Information
+              Systems Agency, Missile Defense Agency, etc.
+"""
 ##
 # Download a series of contract announcements for contracts worth more than
 # $6.5 million.
@@ -7,13 +17,14 @@
 # contracts start at number 391 (October 07, 1994)
 #
 import os
-import warnings
+import re
 
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
 from collections import defaultdict
 from json import dumps
 from joblib import Parallel, delayed
+
 
 def download_announcements(first_id=1842, last_id=5442):
     """
@@ -37,8 +48,8 @@ def download_announcements(first_id=1842, last_id=5442):
         html = page.read()
 
         save_file = save_dir + id_ + ".html"
-        with open(save_file, 'w') as f:
-            f.write(html)
+        with open(save_file, 'w') as file_:
+            file_.write(html)
 
         print "Downloaded ", url
 
@@ -65,45 +76,44 @@ def make_agency_dict(html_file, save_file=None):
         branch = "contracts"
         # newer contracts use bolded, centered paragraphs for agency
         try:
-            for p in pars:
+            for par in pars:
                 # contracting branches are styled (with centering)
-                if 'style' in p.attrs.keys() or 'align' in p.attrs.keys():
-                    branch = p.get_text().strip().lower()
+                if 'style' in par.attrs.keys() or 'align' in par.attrs.keys():
+                    branch = par.get_text().strip().lower()
                     contract_dict[branch] = []
                 else:
-                    text = p.get_text().strip()
+                    text = par.get_text().strip()
                     if text:
                         contract_dict[branch].append(text)
 
         # older ones use h3 tags
-        except UnboundLocalError as e:
-            print e.message
+        except UnboundLocalError as err:
+            print err.message
             # we need to iterate over all the elements in the div with cntrcts
             contracts_div = soup.find_all(class_="PressOpsContentBody")[1]
 
-            for el in contracts_div.descendants:
+            for elem in contracts_div.descendants:
 
-                if el.name == 'h3':
-                    branch = el.get_text().strip().lower()
+                if elem.name == 'h3':
+                    branch = elem.get_text().strip().lower()
                     if branch:
                         contract_dict[branch] = []
 
-                elif el.name == 'p' and branch:
-                    text = el.get_text().strip()
+                elif elem.name == 'p' and branch:
+                    text = elem.get_text().strip()
                     if text:
                         contract_dict[branch].append(text)
 
     date = \
-        ','.join(map(lambda s: s.strip(),
-                 soup.title.get_text().strip().split(',')[-2:])
-                 )
+        ','.join([s.strip() for s in
+                  soup.title.get_text().strip().split(',')[-2:]])
 
     # key entire dictionary on the date; we'll use it for building time series
     contract_dict = {"date": date, "contracts": contract_dict}
 
     if save_file:
-        with open(save_file, 'w+') as f:
-            f.write(dumps(contract_dict))
+        with open(save_file, 'w+') as file_:
+            file_.write(dumps(contract_dict))
 
     return contract_dict
 
@@ -122,8 +132,7 @@ def make_agency_dicts(html_dir="data/html", output_json_dir="data/json",
 
     input_files = [html_dir + f for f in os.listdir(html_dir)
                    if os.path.isfile(html_dir + f)
-                   and not f.startswith('.')
-                   ]
+                   and not f.startswith('.')]
 
     if output_json_dir[-1] != '/':
         output_json_dir += '/'
@@ -131,7 +140,34 @@ def make_agency_dicts(html_dir="data/html", output_json_dir="data/json",
     if parallel:
         Parallel(n_jobs=8)(delayed(make_agency_dict)(f) for f in input_files)
     else:
-        for f in input_files:
-            print f
-            basename = os.path.basename(f).split('.')[0] + '.json'
-            make_agency_dict(f, output_json_dir + basename)
+        for file_ in input_files:
+            print file_
+            basename = os.path.basename(file_).split('.')[0] + '.json'
+            make_agency_dict(file_, output_json_dir + basename)
+
+
+def unzip_fgdc():
+    """
+    Assume a directory structure as can be understood from the simple code
+    below.
+    """
+    to_dir = "data/fgdc/"
+    from_dir = "data/archive/"
+
+    for file_ in os.listdir(from_dir):
+        full_to_dir = to_dir + re.findall("20[01][0-9]", file_)[0]
+        if not os.path.isdir(full_to_dir):
+            os.makedirs(full_to_dir)
+        os.popen("unzip " + from_dir + "'" + file_ + "'" +
+                 " -d " + full_to_dir)
+
+
+def count_by_activity(contract_json):
+    """
+    Given a full JSON contracts dictionary, return a new dictionary of counts
+    by activity.
+    """
+    by_activity = contract_json['contracts']
+    activities = by_activity.keys()
+
+    return dict(zip(activities, [len(by_activity[a]) for a in activities]))
